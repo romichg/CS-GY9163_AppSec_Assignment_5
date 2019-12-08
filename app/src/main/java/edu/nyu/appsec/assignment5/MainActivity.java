4793dc2201436fe0bc707e55ea4bc0f6149448e5
RomichG <hello@romich-g.com>
2019-12-08 15:01:07 -0500

    remove location code, remove opening of URL from unknown hosts, removign unecessary imports

--- app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
+++ app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
@@ -1,31 +1,31 @@
 package edu.nyu.appsec.assignment5;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+//import android.Manifest;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.pm.PackageManager;
+//import android.location.Location;
+//import android.location.LocationListener;
+//import android.location.LocationManager;
 import android.net.Uri;
-import android.net.http.SslError;
-import android.support.v4.app.ActivityCompat;
+//import android.net.http.SslError;
+//import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
+//import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
+//import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.SerializablePermission;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+//import java.io.BufferedInputStream;
+//import java.io.IOException;
+//import java.io.SerializablePermission;
+//import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+//import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity {
     private static final String SPELL_CHECK_URL = "http://appsecclass.report:8080/";
     private static final String KNOWN_HOST = "appsecclass.report";
 
@@ -39,15 +39,22 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 return false;
             }
 
+            /* RG>> We should not be opening a browser to a URL we don't recognize.
+             * We just return false and don't do nothing
+
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             startActivity(intent);
+            */
             return true;
         }
     }
 
+    // RG>> No need for the spying location service! We don't spy on people!
+
     /* Get location data to provide language localization
     *  Supported languages ar-DZ zh-CN en-US en-IN en-AU fr-FR
     */
+    /* RG Commenting out spying location service
     @Override
     public void onLocationChanged(Location location) {
         URL url = null;
@@ -69,9 +76,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             e.printStackTrace();
         }
     }
+    */
 
     /* Necessary to implement the LocationListener interface
     */
+    /* RG>> Commenting out spying location service
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {}
 
@@ -80,6 +89,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onProviderDisabled(String s) {}
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,16 +99,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         WebView view = new WebView(this);
         view.setWebViewClient(new MyWebViewClient());
 
-        WebSettings settings = view.getSettings();
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setJavaScriptEnabled(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
+        // WebSettings settings = view.getSettings(); RG>> And since we commented out all below, we don't need this
+        // settings.setAllowFileAccessFromFileURLs(true); RG>> No need to allow file access
+        // settings.setJavaScriptEnabled(true); RG>> No need for java script. There is no JS in our pages
+        // settings.setAllowUniversalAccessFromFileURLs(true); RG >> No need for file access
 
+        /* RG>> There is no need for the spying location service.. naughty
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
+        */
 
         setContentView(view);
         view.loadUrl(SPELL_CHECK_URL + "register");
